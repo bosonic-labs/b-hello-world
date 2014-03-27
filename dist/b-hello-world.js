2554ac13a5056448aefcc6af42deeb248a8d7e25
Raphaël Rougeron <goldoraf@gmail.com>
2014-03-27 11:33:06 +0100

    Use Shadow DOM & CSS

--- dist/b-hello-world.js
+++ dist/b-hello-world.js
@@ -1,13 +1,9 @@
-(function() {
-    
-
-    Bosonic.registerElement(
-        'b-hello-world',
-        {
-    readyCallback: function () {
-        this.appendChild(this.template.content.cloneNode(true));
-    },
-    template: '        <p>Hello world!</p>    '
-}
-    );
+(function () {
+    Bosonic.registerElement('b-hello-world', {
+        readyCallback: function () {
+            var root = this.createShadowRoot();
+            root.appendChild(this.template.content.cloneNode(true));
+        },
+        template: ' <p>Hello world!</p> '
+    });
 }());
