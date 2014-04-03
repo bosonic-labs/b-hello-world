c335f185e4e0d2492424c6348bc771cef13842cb
Raphaël Rougeron <goldoraf@gmail.com>
2014-04-03 17:54:01 +0200

    Updated for 0.4.0

--- dist/b-hello-world.js
+++ dist/b-hello-world.js
@@ -1,9 +1,30 @@
 (function () {
-    Bosonic.registerElement('b-hello-world', {
-        readyCallback: function () {
-            var root = this.createShadowRoot();
-            root.appendChild(this.template.content.cloneNode(true));
-        },
-        template: ' <p>Hello world!</p> '
+    var BHelloWorldPrototype = Object.create(HTMLElement.prototype, {
+            createdCallback: {
+                enumerable: true,
+                value: function () {
+                    var root = this.createShadowRoot();
+                    root.appendChild(this.template.content.cloneNode(true));
+                }
+            }
+        });
+    window.BHelloWorld = document.registerElement('b-hello-world', { prototype: BHelloWorldPrototype });
+    Object.defineProperty(BHelloWorld.prototype, '_super', {
+        enumerable: false,
+        writable: false,
+        configurable: false,
+        value: HTMLElement.prototype
+    });
+    Object.defineProperty(BHelloWorldPrototype, 'template', {
+        get: function () {
+            var fragment = document.createDocumentFragment();
+            var div = fragment.appendChild(document.createElement('div'));
+            div.innerHTML = ' <p>Hello world!</p> ';
+            while (child = div.firstChild) {
+                fragment.insertBefore(child, div);
+            }
+            fragment.removeChild(div);
+            return { content: fragment };
+        }
     });
 }());
